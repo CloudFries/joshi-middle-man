ff78aa673ecf344f3a13dc2ca29f3f14829322c3
CloudFries <59939601+CloudFries@users.noreply.github.com>
2020-07-12 13:41:32 -0400

    Update close.js

--- commands/info/close.js
+++ commands/info/close.js
@@ -5,11 +5,11 @@ module.exports={
     category: 'info',
     description: 'text here',
     run: async(bot,message,args)=>{
-      if (message.channel.parent.name !== "Tickets") return message.reply("You can only use this command on tickets!");
-      let role = database.get(`staff_${message.guild.id}`)
-  let backup = message.guild.roles.cache.find(role => role.name === "Ticket Staff");
-  if(role === null || 0) return message.reply("This guild does not have it's support role set!")
-  if(!message.member.roles.cache.has(role)) return message.reply("You do not have the staff role!")
+            let ticketCat = database.get(`tickets_${message.guild.id}`)
+      
+      if (message.channel.parentID !== ticketCat) {
+        return message.channel.send(errorEmbed.setDescription("This command can only be used in tickets!"))
+}
       message.channel.delete();
     }
 }
